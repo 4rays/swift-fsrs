1fc9004dc5218988ca70c9f1db70953a086ef8c7
Reda Lemeden <git@redalemeden.com>
2024-10-24 15:27:12 +0200

    Use more descriptive variable names in CardReview

--- Sources/SwiftFSRS/Models/CardReview.swift
+++ Sources/SwiftFSRS/Models/CardReview.swift
@@ -2,7 +2,7 @@ import Foundation
 
 public struct CardReview: Codable {
   /// The post-review card.
-  public var card: Card
+  public var postReviewCard: Card
 
   /// The review log.
   public var reviewLog: ReviewLog
@@ -10,20 +10,20 @@ public struct CardReview: Codable {
   /// Initialize a new card review.
   ///
   /// - Parameters:
-  ///   - card: The post-review card.
-  ///   - oldCard: The pre-review card.
+  ///   - postReviewCard: The post-review card.
+  ///   - preReviewCard: The pre-review card.
   ///   - rating: The rating given to the card.
-  public init(card: Card, oldCard: Card, rating: Rating) {
-    self.card = card
+  public init(postReviewCard: Card, preReviewCard: Card, rating: Rating) {
+    self.postReviewCard = postReviewCard
 
     self.reviewLog = .init(
       rating: rating,
-      status: oldCard.status,
-      due: oldCard.due,
-      stability: oldCard.stability,
-      difficulty: oldCard.difficulty,
-      elapsedDays: oldCard.elapsedDays,
-      scheduledDays: oldCard.scheduledDays,
+      status: preReviewCard.status,
+      due: preReviewCard.due,
+      stability: preReviewCard.stability,
+      difficulty: preReviewCard.difficulty,
+      elapsedDays: preReviewCard.elapsedDays,
+      scheduledDays: preReviewCard.scheduledDays,
       reviewedAt: Date()
     )
   }
